d5e7ad441364eb2aaef7531170e54356f432171e
leonm2069 <114175117+leonm2069@users.noreply.github.com>
2023-02-01 14:13:01 -0300

    mi primer proyecto en github

---
+++ src/primeraparte/PrimeraParte.java
@@ -0,0 +1,14 @@
+
+package primeraparte;
+
+import java.util.Scanner;
+
+public class PrimeraParte {
+            
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+        Scanner sc=new Scanner(System.in);
+}
